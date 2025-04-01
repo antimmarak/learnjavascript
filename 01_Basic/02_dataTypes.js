4c9dd260258229bca580d28f4001606cbae75d0f
Mr Antim <75169197+antimnafak-dev@users.noreply.github.com>
2025-04-01 10:03:15 +0000

    Course Update

---
+++ 01_Basic/02_dataTypes.js
@@ -0,0 +1,32 @@
+"use strict"; // treat all JS code as newer version
+
+// alert(3+5) // we are using nodejs,not browser
+
+console.log(3
+    
+    +5) // Code readability should be high(For Example)
+
+    console.log("Antim")
+
+
+let name = "Antim"
+let age = 20
+let isloggedIn = false
+let state;
+
+
+// number => 2 to Power 53
+// bigint
+// string => "preferd"
+// boolean => true/false
+// null => standalone value (empty valu)
+// undefined =>
+// symbol => Unique
+
+
+
+// object 
+
+
+console.log (typeof undefined); //undefined
+console.log (typeof null); //object
