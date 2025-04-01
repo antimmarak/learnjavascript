1b6159723e042a076b6308aeacb541b7e1667e07
Mr Antim <75169197+antimnafak-dev@users.noreply.github.com>
2025-04-01 10:36:22 +0000

    done

---
+++ 01_Basic/03_conversionOperation.js
@@ -0,0 +1,17 @@
+let score = "antim"
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInnumber = Number(score)
+console.log(typeof valueInnumber);
+console.log(valueInnumber);
+
+
+// "33" => 33
+// "33abc" =>NaN
+// true => 1; false =>0
+
+let isloggedIn = 1
+
+let booleanIsloggedIn = Boolean(isloggedIn)
+console.log(booleanIsloggedIn)
